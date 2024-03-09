accd443570b05011aa3d1e20e18e51b2f28d0c4a
mhshahandeh <shahandeh30@gmail.com>
2024-03-09 14:45:32 +0330

    Show popular tags on the rightSidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
 import { getHotQuestions } from "@/lib/actions/question.action";
+import { getPopularTags } from "@/lib/actions/tag.action";
 
 // const hotQuestions = [
 //   {
@@ -26,6 +27,8 @@ const popularTags = [
 
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
+
+  const popularTags = await getPopularTags();
   return (
     <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden w-[350px] custom-scrollbar">
       <div>
@@ -59,7 +62,7 @@ const RightSidebar = async () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
