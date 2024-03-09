a52517cb1b5aa75dd70f5898cb291d462e2db841
mhshahandeh <shahandeh30@gmail.com>
2024-03-09 14:36:55 +0330

    Show top questions on the rightSidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,18 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
 
-const hotQuestions = [
-  {
-    _id: "1",
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)",
-  },
-  { _id: "2", title: "Is it only me or the font is bolder than necessary?" },
-  { _id: "3", title: "Can I get the course for free?" },
-  { _id: "4", title: "Redux Toolkit Not Updating State as Expected" },
-  { _id: "5", title: "Async/Await Function Not Handling Errors Properly" },
-];
+// const hotQuestions = [
+//   {
+//     _id: "1",
+//     title:
+//       "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)",
+//   },
+//   { _id: "2", title: "Is it only me or the font is bolder than necessary?" },
+//   { _id: "3", title: "Can I get the course for free?" },
+//   { _id: "4", title: "Redux Toolkit Not Updating State as Expected" },
+//   { _id: "5", title: "Async/Await Function Not Handling Errors Properly" },
+// ];
 
 const popularTags = [
   { _id: "1", name: "javascript", totalQuestions: 5 },
@@ -23,7 +24,8 @@ const popularTags = [
   { _id: "5", name: "redux", totalQuestions: 10 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
   return (
     <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden w-[350px] custom-scrollbar">
       <div>
@@ -31,7 +33,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
